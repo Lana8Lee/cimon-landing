0601e52adce9906f7f68bfaadd7aae01a1c283ad
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-09-16 13:53:52 +0300

    Menu fix

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".nav__btn").click(function(){
-        $(".nav__list").slideToggle("slow");
+        $(".nav__list").toggle();
         $(".nav__btn__icon").toggleClass("active");
     });
 });
