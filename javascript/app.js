de6accf2e68e4373d42731ba3736f22ddaadb297
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-09-20 12:11:28 +0300

    touched links

--- javascript/app.js
+++ javascript/app.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
         $(".nav__list").toggle();
         $(".nav__btn__icon").toggleClass("active");
     });
+    $(".section__post__btn").on("tap", function(){
+        $(this).addClass("btn--touched");
+    });
+    $(".section__post__btn").on("click", function(){
+        $(this).addClass("btn--touched");
+    });
+    $(".social__link").on("tap", function(){
+        $(this).addClass("btn--touched");
+    });
+    $(".social__link").on("click", function(){
+        $(this).addClass("btn--touched");
+    });
 });
