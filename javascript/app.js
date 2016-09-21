200c7f7424f8e56fb0c24f1b9cdfb6da99a6c41b
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-09-21 13:38:21 +0300

    add tablet version

--- javascript/app.js
+++ javascript/app.js
@@ -8,6 +8,10 @@ $(document).ready(function(){
             $(".nav__list").toggle();
             $(".nav__btn__icon").toggleClass("active");
     });
+    $(".nav__link").click(function(){
+        $(".nav__link").removeClass("nav__link--active");
+        $(this).addClass("nav__link--active");
+    });
     $(".section__post__btn").on("tap",
         function(){
             $(this).addClass("btn--touched");
